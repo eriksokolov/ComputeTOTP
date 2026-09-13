a1914c2766f87e5d2803e1ab0db349ee09828327
eriksokolov <esokolovi@outlook.com>
2026-09-13 21:08:04 +0200

    Update ComputeTOTP.ps1

--- ComputeTOTP.ps1
+++ ComputeTOTP.ps1
@@ -6,5 +6,6 @@ $b="";$s.ToCharArray()|%{$b+=[Convert]::ToString($a.IndexOf($_),2).PadLeft(5,'0'
 $c=[BitConverter]::GetBytes([long]([math]::Floor([DateTimeOffset]::UtcNow.ToUnixTimeSeconds()/30)));
 if([BitConverter]::IsLittleEndian){[array]::Reverse($c)};
 $h=[Security.Cryptography.HMACSHA1]::new($k).ComputeHash($c);
-$o=$h[-1]-band 15;((($h[$o]-band 127)-shl 24)-bor(($h[$o+1]-band 255)-shl 16)-bor(($h[$o+2]-band 255)-shl 8)-bor($h[$o+3]-band 255))%1000000 -as [int] |
+$o=$h[-1]-band 15;
+((($h[$o]-band 127)-shl 24)-bor(($h[$o+1]-band 255)-shl 16)-bor(($h[$o+2]-band 255)-shl 8)-bor($h[$o+3]-band 255))%1000000 -as [int] |
 % ToString D6
